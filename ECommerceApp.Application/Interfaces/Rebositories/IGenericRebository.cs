c043e850d58835d6edc2295992f688147e2fbff4
ahmedkamalyoussef <ahmedkamalyoussef4@gmail.com>
2026-02-25 00:27:35 +0200

    Finish Product Features and edit Geniric Repo

--- ECommerceApp.Application/Interfaces/Rebositories/IGenericRebository.cs
+++ ECommerceApp.Application/Interfaces/Rebositories/IGenericRebository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace ECommerceApp.Application.Interfaces.Rebositories
@@ -7,6 +8,8 @@ namespace ECommerceApp.Application.Interfaces.Rebositories
     public interface IGenericRebository<T>
     {
         public IQueryable<T> GetAll();
+        public IQueryable<T> FindAsync(Expression<Func<T,bool>> expression);
+        public Task<T> GetByIdAsyc(int id);
         public Task Add(T entity);
         public Task Update(T entity);
         public Task Delete(T entity);
--- ECommerceApp.Infrastructure/Repositories/GenericRebository.cs
+++ ECommerceApp.Infrastructure/Repositories/GenericRebository.cs
@@ -1,6 +1,8 @@
 ﻿using ECommerceApp.Application.Interfaces.Rebositories;
 using ECommerceApp.Domain;
 using ECommerceApp.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace ECommerceApp.Infrastructure.Repositories
 {
@@ -25,10 +27,33 @@ namespace ECommerceApp.Infrastructure.Repositories
 
         }
 
+        public IQueryable<T> FindAsync(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression);
+        }
+
         public IQueryable<T> GetAll()
         {
             var result = _context.Set<T>();
             return result;
+        }
+
+        public async Task<T?> GetByIdAsync(
+             int id,
+             List<Expression<Func<T, object>>>? includes = null
+            )
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            return await query.FirstOrDefaultAsync(e =>e.Id == id);
         }
 
         public async Task Update(T entity)
