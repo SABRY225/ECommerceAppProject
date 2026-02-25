305e813e8edf3f5560254072c9dba3d6797c1462
Sultan Ali <sultan.alhejeery@gmail.com>
2026-02-25 10:46:13 +0200

    Add Admin details

---
+++ ECommerceApp.Application/DTOs/AdminDto/AddAdminDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Application.DTOs.AdminDto
+{
+    public class AddAdminDto
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        
+        public string Password { get; set; }
+        [Compare("Password")]
+        public string ConFirmPassword { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    }
+}
---
+++ ECommerceApp.Application/DTOs/AdminDto/GetCustomerDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Application.DTOs.AdminDto
+{
+    public class GetCustomerDto
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; } 
+        public int Total_Order { get; set; }
+
+    }
+}
